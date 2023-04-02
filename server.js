ad5d302f7ac833cb722a682b09ad94f45615d1bb
Dru Kelly <drukelly@gmail.com>
2023-04-01 22:10:50 -0700

    Reduce raw data from API down to team name, wins and losses

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
 const express = require('express');
 const path = require('path');
-const livereload = require('livereload');
-const connectLivereload = require('connect-livereload');
 
-const data = require('./data/nba-standings.json');
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'static'));
-
-liveReloadServer.server.once('connection', () => {
-  setTimeout(() => {
-    liveReloadServer.refresh('/');
-  }, 100);
-});
+const west = require('./data/western-conference.json');
+const east = require('./data/eastern-conference.json');
 
 const app = express();
 const port = 3000;
 
+app.set('trust proxy', 1);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-app.get('/data', (req, res) => {
-  res.json(data);
-});
-
-app.use(connectLivereload());
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'NBA Standings' });
+app.get('/', (request, response) => {
+  response.render('pages/index', { pageTitle: "NBA Standings", data: { west, east } });
+});
+
+app.get('/conference/west', (request, response) => {
+  response.json(west);
+});
+
+app.get('/conference/east', (request, response) => {
+  response.json(east);
 });
 
 app.listen(port, () => {
--- src/generate-nba-standings.py
+++ src/generate-nba-standings.py
@@ -10,59 +10,56 @@ load_dotenv()
 rapid_api_key = os.getenv("RAPID_API_KEY")
 rapid_api_host = os.getenv("RAPID_API_HOST")
 
-conn = http.client.HTTPSConnection("api-basketball.p.rapidapi.com")
+conn = http.client.HTTPSConnection(rapid_api_host)
 
-headers = {
-    'X-RapidAPI-Key': rapid_api_key,
-    'X-RapidAPI-Host': rapid_api_host
-}
+headers = {'X-RapidAPI-Key': rapid_api_key, 'X-RapidAPI-Host': rapid_api_host}
 
 today = date.today()
 this_year = today.year
 last_year = this_year - 1
 season_years = str(last_year) + "-" + str(this_year)
 
-conn.request("GET", "/standings?league=12&season=" +
-             season_years, headers=headers)
+conn.request("GET",
+             "/standings?league=12&season=" + season_years,
+             headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-# prints the data
-# print(data.decode("utf-8"))
-
-# writes the data to a file
-f = open("data/nba-standings.json", "w")
-f.write(data.decode("utf-8"))
-f.close()
+western_conf = []
+eastern_conf = []
 
 
 def main():
     theJSON = json.loads(data.decode("utf-8"))
-    print("NBA Standings: " + season_years)
-    print("Western Conference Standings:")
-    print("=====================================")
-    seed = 0
-    for i in theJSON["response"][0]:
-        if i["group"]["name"] == "Western Conference":
-            seed += 1
-            print(str(seed) + ". " + i["team"]["name"])
-            if seed == 6:
-                print("---------------------")
-            if seed == 10:
-                print("---------------------")
+    theJSON = theJSON["response"][0]
 
-    print("\nEastern Conference Standings:")
-    print("=====================================")
-    seed = 0
-    for i in theJSON["response"][0]:
-        if i["group"]["name"] == "Eastern Conference":
-            seed += 1
-            print(str(seed) + ". " + i["team"]["name"])
-            if seed == 6:
-                print("---------------------")
-            if seed == 10:
-                print("---------------------")
+    for team in theJSON:
+        if team["group"]["name"] == "Western Conference":
+            western_conf.append({
+                "team": team["team"]["name"],
+                "wins": team["games"]["win"]["total"],
+                "losses": team["games"]["lose"]["total"]
+            })
+
+        elif team["group"]["name"] == "Eastern Conference":
+            eastern_conf.append({
+                "team": team["team"]["name"],
+                "wins": team["games"]["win"]["total"],
+                "losses": team["games"]["lose"]["total"]
+            })
+
+    with open("data/western-conference-teams.json", "w") as wc:
+        json.dump(western_conf, wc, indent=4)
+    with open("data/eastern-conference-teams.json", "w") as ec:
+        json.dump(eastern_conf, ec, indent=4)
+
+    wc.close()
+    ec.close()
+
+    conn.close()
+
+    print("Western and Eastern conference JSON files generated.")
 
 
 if __name__ == "__main__":
